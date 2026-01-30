75a2c49f2c051c495983ee4240ffbc821a715952
developerakkoo <developerakshayjadhav@gmail.com>
2026-01-30 19:05:12 +0530

    new

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,7 +2,8 @@ const admin = require('firebase-admin');
 const path = require('path');
 
 if (!admin.apps.length) {
-  const serviceAccountPath = path.join(process.cwd(), 'firebase.json');
+  const serviceAccountFileName = process.env.FIREBASE_SERVICE_ACCOUNT_FILE || 'magic-formula-d55e8-firebase-adminsdk-fbsvc-f0f70a2c7c.json';
+  const serviceAccountPath = path.join(process.cwd(), serviceAccountFileName);
   const serviceAccount = require(serviceAccountPath);
 
   admin.initializeApp({
