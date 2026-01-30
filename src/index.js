376f39571ac487b15598f1a973c8c8280db3c354
developerakkoo <developerakshayjadhav@gmail.com>
2026-01-30 19:43:54 +0530

    new

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
   try {
     await connectDB();
 
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
       console.log(`🚀 Server running on port ${PORT}`);
     });
   } catch (error) {
